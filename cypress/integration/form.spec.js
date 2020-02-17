5eba9f18ed36b108eb21b580459187271faf9331
zhu5201314 <46183853+zhu5201314@users.noreply.github.com>
2020-02-17 18:59:02 +0800

    Update form.spec.js

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -3,5 +3,23 @@ describe("Form test", () => {
     cy.visit("/");
     cy.get("form");
     cy.screenshot({log: true});
+    cy.screenshot('my-screenshot', {
+     onAfterScreenshot ($el, props) {
+    // props has information about the screenshot,
+    // including but not limited to the following:
+
+     {
+       name: 'my-screenshot',
+       size: '15 kb',
+       dimensions: {
+         width: 1200,
+         height: 1000,
+       },
+       scaled: true,
+       blackout: [],
+       duration: 2300,
+     }
+  },
+})
   });
 });
